f6913a46dd99021a2b243a5991f509ab947bf7f8
samarthahegde <samartha.hegde@amagi.com>
2023-01-18 10:51:39 +0530

    app.go committed

--- app/app.go
+++ app/app.go
@@ -7,9 +7,11 @@ import (
 
 func Start() {
 
+	mux := http.NewServeMux()
+
 	// define routes
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
+	mux.HandleFunc("/greet", greet)
+	mux.HandleFunc("/customers", getAllCustomers)
 
 	//start serverd
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
