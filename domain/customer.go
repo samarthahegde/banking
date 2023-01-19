d4d72cb5f3accdfd4661fdd68b663daad69459c0
samarthahegde <samartha.hegde@amagi.com>
2023-01-19 12:57:34 +0530

    after customer.go

---
+++ domain/customer.go
@@ -0,0 +1,22 @@
+package domain
+
+type Customer struct {
+	Id          string
+	Name        string
+	City        string
+	Zipcode     string
+	DateofBirth string
+	Status      string
+}
+
+type CustomerRepository interface {
+	FindAll() ([]Customer, error)
+}
+
+type CustomerRepositoryStub struct {
+	customers []Customer
+}
+
+func (s CustomerRepositoryStub) FindAll() ([]Customer, error) {
+	return s.customers, nil
+}
